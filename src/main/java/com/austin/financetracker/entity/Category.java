a0cb6a910dbb7825cbfde1d3d9ebb446a479c48f
Austin Nguyen <austin11@uw.edu>
2025-07-11 03:49:13 -0700

    feat: complete Category entity for categories table

---
+++ src/main/java/com/austin/financetracker/entity/Category.java
@@ -0,0 +1,53 @@
+package com.austin.financetracker.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity     // Tells SB this class represents a database table
+@Table(name = "categories")     // Table name
+public class Category {
+    
+    @Id     // Marks the following filed as a primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // Auto-increments the id
+    private Long id;    // id field
+
+    @Column(nullable = false, unique = true)    // Column where name is required and it must be unique
+    private String name;
+    private String description;
+
+    public Category() {}    // Default constructor required by JPA
+
+    public Category(String name, String description) {      // Constructor for creating new categories
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
