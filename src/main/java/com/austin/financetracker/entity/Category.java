7eecc65a7dfa173f0d8b01cf14d07aefa9cedb27
Austin Nguyen <austin11@uw.edu>
2025-07-17 03:01:22 -0700

    feat: add getType() and setType() in Category.java

--- src/main/java/com/austin/financetracker/entity/Category.java
+++ src/main/java/com/austin/financetracker/entity/Category.java
@@ -2,6 +2,8 @@ package com.austin.financetracker.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,12 +24,25 @@ public class Category {
 
     public Category() {}    // Default constructor required by JPA
 
-    public Category(String name, String description) {      // Constructor for creating new categories
+    public Category(String name, String description, TransactionType type) {      // Constructor for creating new categories
         this.name = name;
         this.description = description;
+        this.type = type;
     }
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+
     // Getters and setters to modify field
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
     public Long getId() {
         return id;
     }
