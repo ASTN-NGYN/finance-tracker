e54014dfed2492fad82cb20d0b5b331d1ecf9f27
Austin Nguyen <austin11@uw.edu>
2025-07-23 01:25:19 -0700

    refactor: (2) change the field dependency injection (@Autowired) to a constructor instead for safety and clarity

--- src/main/java/com/austin/financetracker/controller/CategoryController.java
+++ src/main/java/com/austin/financetracker/controller/CategoryController.java
@@ -2,7 +2,6 @@ package com.austin.financetracker.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +18,11 @@ import com.austin.financetracker.service.CategoryService;
 @RequestMapping("/categories")
 public class CategoryController {
 
-    @Autowired
-    private CategoryService categoryService;
+    private final CategoryService categoryService;
     
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     // GET /categories
     @GetMapping
--- src/main/java/com/austin/financetracker/controller/TransactionController.java
+++ src/main/java/com/austin/financetracker/controller/TransactionController.java
@@ -2,7 +2,6 @@ package com.austin.financetracker.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +18,12 @@ import com.austin.financetracker.service.TransactionService;
 @RequestMapping
 public class TransactionController {
 
-    @Autowired
-    private TransactionService transactionService;
+    private final TransactionService transactionService;
 
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    
     // GET /transactions
     @GetMapping
     public List<Transaction> getAllTransactions() {
