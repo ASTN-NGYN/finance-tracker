8170f6513dfe90de318ee209c160d4c9dfdca163
Austin Nguyen <austin11@uw.edu>
2025-08-07 23:47:12 -0700

    feat: add basic app structure: Router, main, routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <main>
+        <Routes>
+          
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
