2fa66f1825427c5c2161e108c2d4b352104d975f
Austin Nguyen <austin11@uw.edu>
2025-08-08 00:07:46 -0700

    fix: install react-dom and import dependencies in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import CategoriesPage from './pages/CategoriesPage';
+import Dashboard from './pages/Dashboard';
+import TransactionsPage from './pages/TransactionsPage';
 
 function App() {
   return (
     <Router>
       <main>
         <Routes>
-          
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+          <Route path="/categories" element={<CategoriesPage/>} />
         </Routes>
       </main>
     </Router>
