46e1aff82928cf47d211b56ca9ea176121e74ce7
Austin Nguyen <austin11@uw.edu>
2025-08-26 02:29:39 -0700

    feat: add api in frontend for getting categories and creating transactions

--- frontend/src/app/utils/api.ts
+++ frontend/src/app/utils/api.ts
@@ -14,6 +14,21 @@ export interface TransactionWithCategoryDTO {
     categoryName: string;
 }
 
+export interface TransactionDTO {
+    amount: number;
+    description: string;
+    date: string;
+    type: string;
+    categoryId: number;
+}
+
+export interface CategoryDTO {
+    id: number;
+    name: string;
+    description: string;
+    type: string;
+}
+
 export async function getTotalIncome(): Promise<number> {
     try {
         const res = await api.get("/transactions/total-income");
@@ -51,6 +66,26 @@ export async function getTransactionWithCategory(): Promise<TransactionWithCateg
     } catch (err) {
         console.error("Error fetching transactions with categories:", err);
         return [];
+    }
+}
+
+export async function getCategories(): Promise<CategoryDTO[]> {
+    try {
+        const res = await api.get("/categories");
+        return res.data;
+    } catch (err) {
+        console.error("Error fetching all categories:", err);
+        return [];
+    }
+}
+
+export async function createTransaction(transactions: TransactionDTO) {
+    try {
+        const res = await api.post("/transactions", transactions)
+        return res.data
+    } catch (err) {
+        console.error("Error creating transactions:", err)
+        throw err
     }
 }
 
